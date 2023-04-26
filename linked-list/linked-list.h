d2cb44f8f102b155af852c4f7217fc9edd17ab0d
Jacob <boderajacob@gmail.com>
2023-04-26 14:10:33 -0400

    Started implementing methods for linked lists

--- linked-list/linked-list.h
+++ linked-list/linked-list.h
@@ -8,12 +8,50 @@ class LinkedList {
 
     private:
 
-        DataType *data;
-        unsigned int capacity;
-        unsigned int size;
+        DataType *data_;
+        unsigned int capacity_;
+        unsigned int size_;
 
         LinkedList(const LinkedList& rhs);
         LinkedList& operator=(const LinkedList& rhs);
+    
+    public: 
+        // Constuctor
+        LinkedList(unsigned int cap);
+        // Destructor
+        ~LinkedList();
+
+        // Constant member functions
+        unsigned int size() const;
+        
+        unsigned int capacity() const;
+        
+        bool empty() const;
+        
+        bool full() const;
+        
+        DataType select(unsigned int index) const;
+
+        unsigned int search(DataType value) const;
+
+        void print() const;
+
+        // Non-constant member functions
+        bool insert(DataType value, unsigned int index);
+
+        bool insertFront(DataType value);
+
+        bool insertBack(DataType value);
+
+        bool remove(unsigned int index);
+
+        bool removeFront(unsigned int index);
+
+        bool removeBack(unsigned int index);
+
+        bool replace(unsigned int index, DataType value);
+
+
 };
 
 
